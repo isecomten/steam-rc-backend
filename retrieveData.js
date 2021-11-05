364303519fdfb24063fd0c5b623d5f7b763579d8
isecomten <93753798+isecomten@users.noreply.github.com>
2021-11-05 20:47:01 +0800

    Update retrieveData.js

--- retrieveData.js
+++ retrieveData.js
@@ -41,8 +41,31 @@ const retrieveData = {
             counter++;
         }
     },
-    ProcessInsertGameReviews: async function () {
+    ProcessInsertGameReviews: async function (initialChunk) {
+        let games = await GetGamesFromDB();
+        games = games.slice(initialChunk);
+        let counter = 0;
 
+        for (let game of games) {
+            await Sleep(1300);
+            console.log(`current chunk => ${initialChunk + counter} appid => ${game.appid}`);
+            try {
+                let gameReviewDetails = await GetGameReviewsFromSteam(game);
+                //response might be successful but app doesn't exist
+                if (gameReviewDetails?.success == false) {
+                    counter++;
+                    continue;
+                }
+                await InsertGameReviewsIntoDB(game, gameReviewDetails);
+            }
+            catch (err) {
+                console.log(err);
+                console.log(`chunk failed on => ${initialChunk + counter}`);
+                this.ProcessInsertGameReviews(initialChunk + counter);
+                return;
+            }
+            counter++;
+        }
     }
 }
 
@@ -142,6 +165,85 @@ async function InsertAppDetailsIntoDB(app, appDetails) {
     try {
         const results = await pool.query(query, values);
         console.log(`Inserted ${app.name} details into database.`);
+    }
+    catch (err) {
+        throw err;
+    }
+}
+async function GetGamesFromDB() {
+    const query = "SELECT * FROM game_details WHERE type 'game' = ORDER BY appid";
+    try {
+        const results = await pool.query(query);
+        return results.rows;
+    }
+    catch (err) {
+        throw (err);
+    }
+}
+async function GetGameReviewsFromSteam(game) {
+    try {
+        const results = await axios.get(`https://store.steampowered.com/appreviews/${game.appid}?json=1&num_per_page=0&language=all`);
+        return results.data;
+    }
+    catch (err) {
+        throw (err);
+    }
+}
+async function InsertGameReviewsIntoDB(game, gameReviewDetails) {
+    const query = 'INSERT INTO game_details\
+    (appid,\
+    name,\
+    required_age,\
+    is_free,\
+    detailed_description,\
+    about_the_game,\
+    short_description,\
+    supported_languages,\
+    header_image,\
+    website,\
+    developers,\
+    publishers,\
+    initial_price,\
+    final_price,\
+    discount_percent,\
+    platforms_windows,\
+    platforms_mac,\
+    platforms_linux,\
+    metacritic_score,\
+    recommendations,\
+    release_date,\
+    type)\
+    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22)\
+    ON CONFLICT DO NOTHING';
+
+    const values = [
+        app.appid,
+        appDetails?.data?.name,
+        appDetails?.data?.required_age,
+        appDetails?.data?.is_free,
+        appDetails?.data?.detailed_description,
+        appDetails?.data?.about_the_game,
+        appDetails?.data?.short_description,
+        appDetails?.data?.supported_languages,
+        appDetails?.data?.header_image,
+        appDetails?.data?.website,
+        appDetails?.data?.developers,
+        appDetails?.data?.publishers,
+        appDetails?.data?.price_overview?.initial,
+        appDetails?.data?.price_overview?.final,
+        appDetails?.data?.price_overview?.discount_percent,
+        appDetails?.data?.platforms?.windows,
+        appDetails?.data?.platforms?.mac,
+        appDetails?.data?.platforms?.linux,
+        appDetails?.data?.metacritic?.score,
+        appDetails?.data?.recommendations?.total,
+        appDetails?.data?.release_date?.date,
+        appDetails?.data?.type
+    ];
+
+    try {
+        const results = await pool.query(query, values);
+        console.log(`Inserted ${game.name} review details into database.`);
     }
     catch (err) {
         throw err;
